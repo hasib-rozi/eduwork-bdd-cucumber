71ff0fc666cd04078f012a5667c29bc2bce6fff1
hasibrozi <hasib.rozi@gmail.com>
2023-03-02 17:19:17 +0700

    change website sample testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,20 @@
 const { defineConfig } = require("cypress");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const addCucumberPreprocessorPlugin =
-  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
-const createEsbuildPlugin =
-  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", browserify.default(config));
+
+  // Make sure to return the config object as it might have been modified by the plugin.
+  return config;
+}
 
 module.exports = defineConfig({
   e2e: {
-    async setupNodeEvents(on, config) {
-      const bundler = createBundler({
-        plugins: [createEsbuildPlugin(config)],
-      });
-
-      on("file:preprocessor", bundler);
-      await addCucumberPreprocessorPlugin(on, config);
-
-      return config;
-    },
-    specPattern: "cypress/e2e/features/*.feature",
-    chromeWebSecurity: false,
+    specPattern: "**/*.feature",
+    setupNodeEvents,
   },
 });
--- cypress/e2e/features/zerobank-search.feature
+++
@@ -1,6 +0,0 @@
-Feature: Search data from Zero Bank Web
-  
-  Scenario: Opening Zero Bank Web
-    Given A user opens Zero Bank webpage
-    When A user type "Bills" in the search field and press Enter
-    Then A user should see the result
